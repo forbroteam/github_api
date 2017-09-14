5dea95cfcc6da90d53b92df2393dd8779918b1bd
bortni <andrewbortnichuk@gmail.com>
2017-09-14 11:30:56 +0200

    unit tests body created

---
+++ app/src/test/java/com/bortni/github_api/PresenterLogicUnitTest.java
@@ -0,0 +1,37 @@
+package com.bortni.github_api;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.MockitoAnnotations;
+
+/**
+ * Created by andrewbortnichuk on 14/09/2017.
+ */
+
+public class PresenterLogicUnitTest {
+
+    @Before
+    public void setupPresenter() {
+        MockitoAnnotations.initMocks(this);
+    }
+
+    @Test
+    public void getDataForTheFirstPage() {
+
+    }
+
+    @Test
+    public void getDataForTheNextPage() {
+
+    }
+
+    @Test
+    public void getDataOnTheLastPage() {
+
+    }
+
+    @Test
+    public void getDataOnTheScrollToBottom() {
+
+    }
+}
